c5f03bb411702a25b03119158a7a95ae652b864c
blainehill2001 <blainehill2001@gmail.com>
2024-04-27 10:58:53 -0500

    cleaned up graph.py stubs and had flake8 ignore @503

--- backend/app.py
+++ backend/app.py
@@ -1,9 +1,11 @@
-from flask import Flask, request #type: ignore
 import datetime
+
+from flask import Flask, request  # type: ignore
 
 app = Flask(__name__)
 
-@app.route('/get_time', methods=['GET'])
+
+@app.route("/get_time", methods=["GET"])
 def get_time():
     # Print the request parameters
     print(f"Request Parameters: {request.args}")
@@ -13,5 +15,6 @@ def get_time():
 
     return current_time
 
-if __name__ == '__main__':
-    app.run(debug=True)
+
+if __name__ == "__main__":
+    app.run(debug=False)
